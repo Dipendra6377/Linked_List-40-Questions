4001ae8fdbe07c7f5faa204fd7703427b97b2a7f
Dipendra6377 <91822875+Dipendra6377@users.noreply.github.com>
2023-01-06 06:59:47 +0530

    Create 07_Remove_Duplicate_Element.java

---
+++ 05-January-2023/Question 1 to Question 10/07_Remove_Duplicate_Element.java
@@ -0,0 +1,60 @@
+// 2ms
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp=head;
+        while(temp!=null && temp.next!=null){
+            if(temp.val==temp.next.val){
+                temp.next=temp.next.next;
+            }
+            else{
+                temp=temp.next;
+            }
+        }
+        return head;
+    }
+}
+
+
+
+//0 ms
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode temp=new ListNode();
+        ListNode t=temp;
+        while(head!=null){
+            while(head.next!=null && head.val==head.next.val){
+                head=head.next;
+            }
+            t.next=new ListNode(head.val);
+            t=t.next;
+            head=head.next;
+        }
+        return temp.next;
+    }
+}
