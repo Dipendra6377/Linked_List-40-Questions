5ddbff7bb7c8dcdb732ea6d6c5831d2b2aad47fe
Dipendra6377 <91822875+Dipendra6377@users.noreply.github.com>
2023-01-09 10:16:02 +0530

    Create 05_Intersection_Of_Two_LL.java

---
+++ 08-January-2023/Question 21 to Question 30/05_Intersection_Of_Two_LL.java
@@ -0,0 +1,20 @@
+public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    //boundary check
+    if(headA == null || headB == null) return null;
+    
+    ListNode a = headA;
+    ListNode b = headB;
+    
+    //if a & b have different len, then we will stop the loop after second iteration
+    while( a != b){
+    	//for the end of first iteration, we just reset the pointer to the head of another linkedlist
+        a = a == null? headB : a.next;
+        b = b == null? headA : b.next;    
+    }
+    
+    return a;
+}
+
+
+
+//https://leetcode.com/problems/intersection-of-two-linked-lists/solutions/49785/java-solution-without-knowing-the-difference-in-len/
